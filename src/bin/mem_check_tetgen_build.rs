ea2b7d65858a29b219798bead1880619b87a5add
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-06-26 18:34:21 +1000

    Add test

--- src/bin/mem_check_tetgen_build.rs
+++ src/bin/mem_check_tetgen_build.rs
@@ -29,6 +29,7 @@ fn run_all() -> Result<(), StrError> {
     set_hole_captures_some_errors()?;
     generate_methods_capture_some_errors()?;
     generate_delaunay_works()?;
+    generate_mesh_works_1()?;
     Ok(())
 }
 
@@ -130,12 +131,113 @@ fn generate_methods_capture_some_errors() -> Result<(), StrError> {
 
 fn generate_delaunay_works() -> Result<(), StrError> {
     let mut tetgen = Tetgen::new(4, None, None, None)?;
-    tetgen.set_point(0, 0.0, 0.0, 0.0)?;
-    tetgen.set_point(1, 1.0, 0.0, 0.0)?;
-    tetgen.set_point(2, 0.0, 1.0, 0.0)?;
-    tetgen.set_point(3, 0.0, 0.0, 1.0)?;
+    tetgen
+        .set_point(0, 0.0, 0.0, 0.0)?
+        .set_point(1, 1.0, 0.0, 0.0)?
+        .set_point(2, 0.0, 1.0, 0.0)?
+        .set_point(3, 0.0, 0.0, 1.0)?;
     tetgen.generate_delaunay(false)?;
     assert_eq!(tetgen.ntet(), 1);
     assert_eq!(tetgen.npoint(), 4);
+    Ok(())
+}
+
+fn generate_mesh_works_1() -> Result<(), StrError> {
+    let mut tetgen = Tetgen::new(
+        16,
+        Some(vec![
+            4, 4, 4, 4, 4, 4, // inner cube
+            4, 4, 4, 4, 4, 4, // outer cube
+        ]),
+        Some(1),
+        Some(1),
+    )?;
+    // inner cube
+    tetgen
+        .set_point(0, 0.0, 0.0, 0.0)?
+        .set_point(1, 1.0, 0.0, 0.0)?
+        .set_point(2, 1.0, 1.0, 0.0)?
+        .set_point(3, 0.0, 1.0, 0.0)?
+        .set_point(4, 0.0, 0.0, 1.0)?
+        .set_point(5, 1.0, 0.0, 1.0)?
+        .set_point(6, 1.0, 1.0, 1.0)?
+        .set_point(7, 0.0, 1.0, 1.0)?;
+    // outer cube
+    tetgen
+        .set_point(8, -1.0, -1.0, -1.0)?
+        .set_point(9, 2.0, -1.0, -1.0)?
+        .set_point(10, 2.0, 2.0, -1.0)?
+        .set_point(11, -1.0, 2.0, -1.0)?
+        .set_point(12, -1.0, -1.0, 2.0)?
+        .set_point(13, 2.0, -1.0, 2.0)?
+        .set_point(14, 2.0, 2.0, 2.0)?
+        .set_point(15, -1.0, 2.0, 2.0)?;
+    // inner cube
+    tetgen
+        .set_facet_point(0, 0, 0)?
+        .set_facet_point(0, 1, 4)?
+        .set_facet_point(0, 2, 7)?
+        .set_facet_point(0, 3, 3)?;
+    tetgen
+        .set_facet_point(1, 0, 1)?
+        .set_facet_point(1, 1, 2)?
+        .set_facet_point(1, 2, 6)?
+        .set_facet_point(1, 3, 5)?;
+    tetgen
+        .set_facet_point(2, 0, 0)?
+        .set_facet_point(2, 1, 1)?
+        .set_facet_point(2, 2, 5)?
+        .set_facet_point(2, 3, 4)?;
+    tetgen
+        .set_facet_point(3, 0, 2)?
+        .set_facet_point(3, 1, 3)?
+        .set_facet_point(3, 2, 7)?
+        .set_facet_point(3, 3, 6)?;
+    tetgen
+        .set_facet_point(4, 0, 0)?
+        .set_facet_point(4, 1, 3)?
+        .set_facet_point(4, 2, 2)?
+        .set_facet_point(4, 3, 1)?;
+    tetgen
+        .set_facet_point(5, 0, 4)?
+        .set_facet_point(5, 1, 5)?
+        .set_facet_point(5, 2, 6)?
+        .set_facet_point(5, 3, 7)?;
+    // outer cube
+    tetgen
+        .set_facet_point(6, 0, 8 + 0)?
+        .set_facet_point(6, 1, 8 + 4)?
+        .set_facet_point(6, 2, 8 + 7)?
+        .set_facet_point(6, 3, 8 + 3)?;
+    tetgen
+        .set_facet_point(7, 0, 8 + 1)?
+        .set_facet_point(7, 1, 8 + 2)?
+        .set_facet_point(7, 2, 8 + 6)?
+        .set_facet_point(7, 3, 8 + 5)?;
+    tetgen
+        .set_facet_point(8, 0, 8 + 0)?
+        .set_facet_point(8, 1, 8 + 1)?
+        .set_facet_point(8, 2, 8 + 5)?
+        .set_facet_point(8, 3, 8 + 4)?;
+    tetgen
+        .set_facet_point(9, 0, 8 + 2)?
+        .set_facet_point(9, 1, 8 + 3)?
+        .set_facet_point(9, 2, 8 + 7)?
+        .set_facet_point(9, 3, 8 + 6)?;
+    tetgen
+        .set_facet_point(10, 0, 8 + 0)?
+        .set_facet_point(10, 1, 8 + 3)?
+        .set_facet_point(10, 2, 8 + 2)?
+        .set_facet_point(10, 3, 8 + 1)?;
+    tetgen
+        .set_facet_point(11, 0, 8 + 4)?
+        .set_facet_point(11, 1, 8 + 5)?
+        .set_facet_point(11, 2, 8 + 6)?
+        .set_facet_point(11, 3, 8 + 7)?;
+    tetgen.set_region(0, -0.9, -0.9, -0.9, 1, None)?;
+    tetgen.set_hole(0, 0.5, 0.5, 0.5)?;
+    tetgen.generate_mesh(false, false, None, None)?;
+    assert_eq!(tetgen.ntet(), 116);
+    assert_eq!(tetgen.npoint(), 50);
     Ok(())
 }
--- src/tetgen.rs
+++ src/tetgen.rs
@@ -754,10 +754,11 @@ mod tests {
     #[test]
     fn generate_delaunay_works() -> Result<(), StrError> {
         let mut tetgen = Tetgen::new(4, None, None, None)?;
-        tetgen.set_point(0, 0.0, 0.0, 0.0)?;
-        tetgen.set_point(1, 1.0, 0.0, 0.0)?;
-        tetgen.set_point(2, 0.0, 1.0, 0.0)?;
-        tetgen.set_point(3, 0.0, 0.0, 1.0)?;
+        tetgen
+            .set_point(0, 0.0, 0.0, 0.0)?
+            .set_point(1, 1.0, 0.0, 0.0)?
+            .set_point(2, 0.0, 1.0, 0.0)?
+            .set_point(3, 0.0, 0.0, 1.0)?;
         tetgen.generate_delaunay(false)?;
         assert_eq!(tetgen.ntet(), 1);
         assert_eq!(tetgen.npoint(), 4);
@@ -767,10 +768,11 @@ mod tests {
     #[test]
     fn draw_wireframe_works() -> Result<(), StrError> {
         let mut tetgen = Tetgen::new(4, None, None, None)?;
-        tetgen.set_point(0, 0.0, 0.0, 0.0)?;
-        tetgen.set_point(1, 1.0, 0.0, 0.0)?;
-        tetgen.set_point(2, 0.0, 1.0, 0.0)?;
-        tetgen.set_point(3, 0.0, 0.0, 1.0)?;
+        tetgen
+            .set_point(0, 0.0, 0.0, 0.0)?
+            .set_point(1, 1.0, 0.0, 0.0)?
+            .set_point(2, 0.0, 1.0, 0.0)?
+            .set_point(3, 0.0, 0.0, 1.0)?;
         tetgen.generate_delaunay(false)?;
         assert_eq!(tetgen.ntet(), 1);
         assert_eq!(tetgen.npoint(), 4);
@@ -787,14 +789,15 @@ mod tests {
     #[test]
     fn generate_delaunay_works_1() -> Result<(), StrError> {
         let mut tetgen = Tetgen::new(8, None, None, None)?;
-        tetgen.set_point(0, 0.0, 0.0, 0.0)?;
-        tetgen.set_point(1, 1.0, 0.0, 0.0)?;
-        tetgen.set_point(2, 1.0, 1.0, 0.0)?;
-        tetgen.set_point(3, 0.0, 1.0, 0.0)?;
-        tetgen.set_point(4, 0.0, 0.0, 1.0)?;
-        tetgen.set_point(5, 1.0, 0.0, 1.0)?;
-        tetgen.set_point(6, 1.0, 1.0, 1.0)?;
-        tetgen.set_point(7, 0.0, 1.0, 1.0)?;
+        tetgen
+            .set_point(0, 0.0, 0.0, 0.0)?
+            .set_point(1, 1.0, 0.0, 0.0)?
+            .set_point(2, 1.0, 1.0, 0.0)?
+            .set_point(3, 0.0, 1.0, 0.0)?
+            .set_point(4, 0.0, 0.0, 1.0)?
+            .set_point(5, 1.0, 0.0, 1.0)?
+            .set_point(6, 1.0, 1.0, 1.0)?
+            .set_point(7, 0.0, 1.0, 1.0)?;
         tetgen.generate_delaunay(false)?;
         assert_eq!(tetgen.ntet(), 6);
         assert_eq!(tetgen.npoint(), 8);
@@ -804,6 +807,114 @@ mod tests {
             plot.set_equal_axes(true)
                 .set_figure_size_points(600.0, 600.0)
                 .save("/tmp/tritet/tetgen_test_delaunay_1.svg")?;
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn generate_mesh_works_1() -> Result<(), StrError> {
+        let mut tetgen = Tetgen::new(
+            16,
+            Some(vec![
+                4, 4, 4, 4, 4, 4, // inner cube
+                4, 4, 4, 4, 4, 4, // outer cube
+            ]),
+            Some(1),
+            Some(1),
+        )?;
+        // inner cube
+        tetgen
+            .set_point(0, 0.0, 0.0, 0.0)?
+            .set_point(1, 1.0, 0.0, 0.0)?
+            .set_point(2, 1.0, 1.0, 0.0)?
+            .set_point(3, 0.0, 1.0, 0.0)?
+            .set_point(4, 0.0, 0.0, 1.0)?
+            .set_point(5, 1.0, 0.0, 1.0)?
+            .set_point(6, 1.0, 1.0, 1.0)?
+            .set_point(7, 0.0, 1.0, 1.0)?;
+        // outer cube
+        tetgen
+            .set_point(8, -1.0, -1.0, -1.0)?
+            .set_point(9, 2.0, -1.0, -1.0)?
+            .set_point(10, 2.0, 2.0, -1.0)?
+            .set_point(11, -1.0, 2.0, -1.0)?
+            .set_point(12, -1.0, -1.0, 2.0)?
+            .set_point(13, 2.0, -1.0, 2.0)?
+            .set_point(14, 2.0, 2.0, 2.0)?
+            .set_point(15, -1.0, 2.0, 2.0)?;
+        // inner cube
+        tetgen
+            .set_facet_point(0, 0, 0)?
+            .set_facet_point(0, 1, 4)?
+            .set_facet_point(0, 2, 7)?
+            .set_facet_point(0, 3, 3)?;
+        tetgen
+            .set_facet_point(1, 0, 1)?
+            .set_facet_point(1, 1, 2)?
+            .set_facet_point(1, 2, 6)?
+            .set_facet_point(1, 3, 5)?;
+        tetgen
+            .set_facet_point(2, 0, 0)?
+            .set_facet_point(2, 1, 1)?
+            .set_facet_point(2, 2, 5)?
+            .set_facet_point(2, 3, 4)?;
+        tetgen
+            .set_facet_point(3, 0, 2)?
+            .set_facet_point(3, 1, 3)?
+            .set_facet_point(3, 2, 7)?
+            .set_facet_point(3, 3, 6)?;
+        tetgen
+            .set_facet_point(4, 0, 0)?
+            .set_facet_point(4, 1, 3)?
+            .set_facet_point(4, 2, 2)?
+            .set_facet_point(4, 3, 1)?;
+        tetgen
+            .set_facet_point(5, 0, 4)?
+            .set_facet_point(5, 1, 5)?
+            .set_facet_point(5, 2, 6)?
+            .set_facet_point(5, 3, 7)?;
+        // outer cube
+        tetgen
+            .set_facet_point(6, 0, 8 + 0)?
+            .set_facet_point(6, 1, 8 + 4)?
+            .set_facet_point(6, 2, 8 + 7)?
+            .set_facet_point(6, 3, 8 + 3)?;
+        tetgen
+            .set_facet_point(7, 0, 8 + 1)?
+            .set_facet_point(7, 1, 8 + 2)?
+            .set_facet_point(7, 2, 8 + 6)?
+            .set_facet_point(7, 3, 8 + 5)?;
+        tetgen
+            .set_facet_point(8, 0, 8 + 0)?
+            .set_facet_point(8, 1, 8 + 1)?
+            .set_facet_point(8, 2, 8 + 5)?
+            .set_facet_point(8, 3, 8 + 4)?;
+        tetgen
+            .set_facet_point(9, 0, 8 + 2)?
+            .set_facet_point(9, 1, 8 + 3)?
+            .set_facet_point(9, 2, 8 + 7)?
+            .set_facet_point(9, 3, 8 + 6)?;
+        tetgen
+            .set_facet_point(10, 0, 8 + 0)?
+            .set_facet_point(10, 1, 8 + 3)?
+            .set_facet_point(10, 2, 8 + 2)?
+            .set_facet_point(10, 3, 8 + 1)?;
+        tetgen
+            .set_facet_point(11, 0, 8 + 4)?
+            .set_facet_point(11, 1, 8 + 5)?
+            .set_facet_point(11, 2, 8 + 6)?
+            .set_facet_point(11, 3, 8 + 7)?;
+        tetgen.set_region(0, -0.9, -0.9, -0.9, 1, None)?;
+        tetgen.set_hole(0, 0.5, 0.5, 0.5)?;
+        tetgen.generate_mesh(false, false, None, None)?;
+        assert_eq!(tetgen.ntet(), 116);
+        assert_eq!(tetgen.npoint(), 50);
+        let mut plot = Plot::new();
+        tetgen.draw_wireframe(&mut plot, true, true, true, true, None, None, None);
+        if false {
+            plot.set_equal_axes(true)
+                .set_figure_size_points(600.0, 600.0)
+                .save("/tmp/tritet/tetgen_test_mesh_1.svg")?;
         }
         Ok(())
     }
