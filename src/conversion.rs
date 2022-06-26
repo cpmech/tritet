1f1395a5ba423ddd785cede3c30c807d114ab35e
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-06-26 12:01:30 +1000

    Remove to_u64

--- src/conversion.rs
+++ src/conversion.rs
@@ -6,17 +6,11 @@ pub(crate) fn to_i32(num: usize) -> i32 {
     i32::try_from(num).unwrap()
 }
 
-/// Converts number to u64
-#[inline]
-pub(crate) fn to_u64(num: usize) -> u64 {
-    u64::try_from(num).unwrap()
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
 mod tests {
-    use super::{to_i32, to_u64};
+    use super::to_i32;
 
     #[test]
     fn usize_to_i32_works() {
@@ -24,13 +18,5 @@ mod tests {
         let x = vec![0.0; m];
         let m_i32 = to_i32(x.len());
         assert_eq!(m_i32, 2_i32);
-    }
-
-    #[test]
-    fn usize_to_u64_works() {
-        let m = 2_usize;
-        let x = vec![0.0; m];
-        let m_u64 = to_u64(x.len());
-        assert_eq!(m_u64, 2_u64);
     }
 }
