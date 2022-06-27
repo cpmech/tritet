337cfd6e8fd265af5f82637e6aaa944d1ac82cf0
Dorival Pedroso <dorival.pedroso@gmail.com>
2022-06-27 14:23:44 +1000

    Add examples

--- examples/README.md
+++ examples/README.md
@@ -17,3 +17,17 @@
 [triangle_mesh_1.rs](https://github.com/cpmech/tritet/tree/main/examples/triangle_mesh_1.rs)
 
 ![example_triangle_mesh_1.svg](https://raw.githubusercontent.com/cpmech/tritet/main/data/figures/example_triangle_mesh_1.svg)
+
+## Tetgen: Delaunay triangulation
+
+[tetgen_delaunay_1.rs](https://github.com/cpmech/tritet/tree/main/examples/tetgen_delaunay_1.rs)
+
+![example_tetgen_delaunay_1.svg](https://raw.githubusercontent.com/cpmech/tritet/main/data/figures/example_tetgen_delaunay_1.svg)
+
+## Tetgen: Mesh generation
+
+[tetgen_mesh_1.rs](https://github.com/cpmech/tritet/tree/main/examples/tetgen_mesh_1.rs)
+
+![example_tetgen_mesh_1.png](https://raw.githubusercontent.com/cpmech/tritet/main/data/figures/example_tetgen_mesh_1.png)
+
+![example_tetgen_mesh_1.svg](https://raw.githubusercontent.com/cpmech/tritet/main/data/figures/example_tetgen_mesh_1.svg)
---
+++ examples/tetgen_delaunay_1.rs
@@ -0,0 +1,29 @@
+use plotpy::Plot;
+use tritet::{StrError, Tetgen};
+
+fn main() -> Result<(), StrError> {
+    // allocate data for 8 points
+    let mut tetgen = Tetgen::new(8, None, None, None)?;
+
+    // set points
+    tetgen
+        .set_point(0, 0.0, 0.0, 0.0)?
+        .set_point(1, 1.0, 0.0, 0.0)?
+        .set_point(2, 1.0, 1.0, 0.0)?
+        .set_point(3, 0.0, 1.0, 0.0)?
+        .set_point(4, 0.0, 0.0, 1.0)?
+        .set_point(5, 1.0, 0.0, 1.0)?
+        .set_point(6, 1.0, 1.0, 1.0)?
+        .set_point(7, 0.0, 1.0, 1.0)?;
+
+    // generate Delaunay triangulation
+    tetgen.generate_delaunay(false)?;
+
+    // draw edges of tetrahedra
+    let mut plot = Plot::new();
+    tetgen.draw_wireframe(&mut plot, true, true, true, true, None, None, None);
+    plot.set_equal_axes(true)
+        .set_figure_size_points(600.0, 600.0)
+        .save("/tmp/tritet/example_tetgen_delaunay_1.svg")?;
+    Ok(())
+}
