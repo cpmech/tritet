c19424c8af19ce922f01622f497c6174a9e20ed0
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-10 17:45:12 +1000

    Add test

--- src/trigen_paraview.rs
+++ src/trigen_paraview.rs
@@ -181,4 +181,49 @@ mod tests {
         );
         Ok(())
     }
+
+    #[test]
+    fn trigen_write_vtu_o2() -> Result<(), StrError> {
+        let mut trigen = Trigen::new(3, Some(3), None, None)?;
+        trigen
+            .set_point(0, 0.0, 0.0)?
+            .set_point(1, 1.0, 0.0)?
+            .set_point(2, 0.0, 1.0)?;
+        trigen
+            .set_segment(0, 0, 1)?
+            .set_segment(1, 1, 2)?
+            .set_segment(2, 2, 0)?;
+        trigen.generate_mesh(false, true, None, None)?;
+        let file_path = "/tmp/tritet/test_trigen_write_vtu_o2.vtu";
+        trigen.write_vtu(file_path)?;
+        let contents = fs::read_to_string(file_path).map_err(|_| "cannot open file")?;
+        assert_eq!(
+            contents,
+            r#"<?xml version="1.0"?>
+<VTKFile type="UnstructuredGrid" version="0.1" byte_order="LittleEndian">
+<UnstructuredGrid>
+<Piece NumberOfPoints="6" NumberOfCells="1">
+<Points>
+<DataArray type="Float64" NumberOfComponents="3" format="ascii">
+0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0 0.0 0.5 0.0 0.0 0.5 0.5 0.0 0.0 0.5 0.0 
+</DataArray>
+</Points>
+<Cells>
+<DataArray type="Int32" Name="connectivity" format="ascii">
+0 1 2 3 4 5 
+</DataArray>
+<DataArray type="Int32" Name="offsets" format="ascii">
+6 
+</DataArray>
+<DataArray type="UInt8" Name="types" format="ascii">
+22 
+</DataArray>
+</Cells>
+</Piece>
+</UnstructuredGrid>
+</VTKFile>
+"#
+        );
+        Ok(())
+    }
 }
