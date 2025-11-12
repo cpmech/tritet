e4f9009083e8b197e48f4c7a892df810e6da0319
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-11-12 13:06:23 +1000

    Impl tool to generate VTU from JSON

--- src/bin/tetgen_mesh.rs
+++ src/bin/tetgen_mesh.rs
@@ -1,44 +1,78 @@
 use plotpy::Plot;
+use std::path::Path;
+use std::path::PathBuf;
+use structopt::StructOpt;
 use tritet::{InputDataTetMesh, StrError, Tetgen};
 
-const SAVE_FIGURE: bool = false;
+/// Command line options
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "tetgen_mesh",
+    about = "Generate tetrahedral mesh using TetGen and export to VTU format"
+)]
+struct Options {
+    /// Input JSON file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+
+    /// Output directory
+    out_dir: String,
+
+    /// Verbose mode
+    #[structopt(short = "V", long)]
+    verbose: bool,
+
+    /// Generate second order tetrahedra
+    #[structopt(short, long)]
+    o2: bool,
+
+    /// Generate SVG figure with the wireframe of the mesh
+    #[structopt(short, long)]
+    svg_figure: bool,
+
+    /// Global maximum volume for tetrahedra
+    #[structopt(short = "v", long)]
+    max_volume: Option<f64>,
+
+    /// Global minimum angle between faces of tetrahedra
+    #[structopt(short = "a", long)]
+    min_angle: Option<f64>,
+}
 
 fn main() -> Result<(), StrError> {
-    // set input data
-    let input_data = InputDataTetMesh {
-        points: vec![
-            (0, 0.0, 1.0, 0.0), // marker, x, y, z
-            (0, 0.0, 0.0, 0.0),
-            (0, 1.0, 1.0, 0.0),
-            (0, 0.0, 1.0, 1.0),
-        ],
-        facets: vec![
-            (0, vec![0, 2, 1]), // marker, point indices
-            (0, vec![0, 1, 3]),
-            (0, vec![0, 3, 2]),
-            (0, vec![1, 2, 3]),
-        ],
-        holes: vec![],                           // no holes
-        regions: vec![(1, 0.1, 0.9, 0.1, None)], // region marker, x, y, z, max volume
-    };
+    // parse options
+    let options = Options::from_args();
+
+    // load input data from JSON file
+    let in_path = Path::new(&options.input);
+    let input_data = InputDataTetMesh::read_json(&in_path)?;
+    let fn_stem = in_path
+        .file_stem()
+        .ok_or("cannot get file stem")?
+        .to_str()
+        .ok_or("cannot convert file stem to str")?;
 
     // allocate generator from input data
     let tetgen = Tetgen::from_input_data(&input_data)?;
 
     // generate mesh
-    let global_max_volume = Some(0.5);
-    tetgen.generate_mesh(false, false, global_max_volume, None)?;
+    let verbose = options.verbose;
+    let o2 = options.o2;
+    tetgen.generate_mesh(verbose, o2, options.max_volume, options.min_angle)?;
 
-    // draw edges of tetrahedra
-    if SAVE_FIGURE {
+    // write SVG file with wireframe
+    if options.svg_figure {
         let mut plot = Plot::new();
-        tetgen.draw_wireframe(&mut plot, true, true, true, true, None, None, None);
+        tetgen.draw_wireframe(&mut plot, true, false, false, false, None, None, None);
         plot.set_equal_axes(true)
-            .set_figure_size_points(600.0, 600.0)
-            .save("/tmp/tritet/doc_tetgen_mesh_2.svg")?;
+            .set_figure_size_points(800.0, 800.0)
+            .save(&format!("{}/{}.svg", options.out_dir, fn_stem))?;
+        println!("\nGenerated SVG file: {}/{}.svg", options.out_dir, fn_stem);
     }
 
-    assert_eq!(tetgen.out_ncell(), 7);
-    assert_eq!(tetgen.out_npoint(), 10);
+    // write VTU file
+    let path_vtu = format!("{}/{}.vtu", options.out_dir, fn_stem);
+    tetgen.write_vtu(&path_vtu)?;
+    println!("\nGenerated VTU file: {}/{}.vtu", options.out_dir, fn_stem);
     Ok(())
 }
---
+++ zscripts/run-tetgen-mesh.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cargo run --bin tetgen_mesh -- -s data/input/example_tet_input.json /tmp/tritet
